27aa947c355b8817f66f679efb2472eff8dba68d
MattGeoSnyder <mattgeosnyder@gmail.com>
2023-05-12 08:21:53 -0400

    Committing backend. Added .env file for config.

---
+++ db.js
@@ -0,0 +1,14 @@
+/** Database connection for Microblog. */
+require('dotenv').config();
+const { Client } = require("pg");
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || "postgresql:///microblog",
+    user: process.env.PGUSERNAME,
+    password: process.env.PGPASSWORD
+});
+
+client.connect();
+
+
+module.exports = client;
